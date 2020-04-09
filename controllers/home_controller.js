c29bbc5773ca94694cc689b32e3fb8d0c064c1b0
Sanya <sanyasharma750344@gmail.com>
2020-04-09 16:44:07 +0530

    Set up static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,8 @@
 module.exports.home = function(req,res)
 {
- return res.end('<h1>Express is up for socialApp</h1>');   
+ return res.render('home',{
+   title: "Home"  
+ });
 }
+
+    
--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expresslayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expresslayouts);
+
+//Extract style & scripts from Sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 //use express router
 app.use('/',require('./routes'));
+
+//set up view
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
 
 
 app.listen(port,function(err)
