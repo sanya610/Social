260a8dbcfb720ea8a77d191599ef50b9f9e0dd68
Sanya <sanyasharma750344@gmail.com>
2020-04-27 14:34:43 +0530

    Add comments to post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,31 @@
-module.exports.home = function(req,res)
-{
- console.log(req.cookies);
+const Post = require('../models/post');
 
- res.cookie('user_id',25);
+module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
 
- return res.render('home',{
-   title: "Home"  
- });
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+      path: 'comments',
+      populate:{
+       path:'user'   
+      }  
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
 }
 
     
