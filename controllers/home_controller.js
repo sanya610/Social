d343a06b1fa24ee7fcf44426ff3f1e673a93d76d
Sanya <sanyasharma750344@gmail.com>
2020-04-27 16:06:09 +0530

    Add delete buttons for post and comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -19,7 +19,7 @@ module.exports.home = function(req, res){
       populate:{
        path:'user'   
       }  
-    })
+    }) 
     .exec(function(err, posts){
         return res.render('home', {
             title: "Codeial | Home",
